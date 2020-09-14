14469ad3ab77161e498617db775c2c7c15aaeb52
Jatinder Singh <js843@njit.edu>
2020-09-14 21:54:13 +0000

    Add query functionality for a food
    Added the get_food_quote() function that queries
    twitter for quotes relevant to a certain food.
    function returns the tweet content, author name,
    and the date and time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tweepy
-    
+import json
+import random    
 
 def get_secrets():
     secret_api_information = []
@@ -11,9 +12,21 @@ def get_secrets():
     except FileNotFoundError:
         print(".env config file wasn't found!")
         exit(1)
-            
+
+
+def get_food_quote(twitter_api, food_query):
+    twitter_query = api.search("chocolate cake", count=100)
+
+    random_tweet = twitter_query[random.randint(0,100)]
+
+    t = api.get_status(random_tweet.id, tweet_mode="extended")
+    try:
+        return (t.retweeted_status.full_text, f"@{t.retweeted_status.author._json['screen_name']}", t.retweeted_status.created_at)
+    except AttributeError:
+        return (t.full_text, f"@{t.author._json['screen_name']}", t.created_at)
+
+
 api_information = get_secrets()
-print(api_information)
 
 try:
     auth = tweepy.OAuthHandler(api_information[0], api_information[1])
