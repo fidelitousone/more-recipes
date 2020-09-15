13cf6c3d61c98b7c6cb7378350199acc39e08110
Jatinder Singh <js843@njit.edu>
2020-09-15 23:14:50 +0000

    Code refactor, Added CS, updated documentation
    Refactored the code to look a little bit nicer and
    added documentation to one function. Added some CSS
    to the project in preparation to make it look nicer
    in the long run; began updating the README.md to
    describe how to install and configure.

--- README.md
+++ README.md
@@ -2,13 +2,28 @@
 A simple web app to retrieve recipes and quotes relating to them on Twitter.
 
 ## Table of Contents
-* Technologies (#technologies)
-* Setup (#setup)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Technical Difficulties](#technical_difficulties)
 
 ## Technologies
 * Python 3.6.12+
 * pip 18.1+
 * tweepy 3.9.0+
+* Flask 1.1.2+
 
 ## Setup
-(WIP)
+- Clone the repo: <code>git clone https://github.com/fidelitousone/project1-js843.git</code>
+- <code>cd</code> into app directory
+- Install dependencies: <code>pip install flask tweepy</code>
+- Create <code>.env</code>
+- File should be formatted as such:
+```
+export=CONSUMER_API_KEY='TODO'
+export=CONSUMER_SECRET_KEY='TODO'
+export=APP_SECRET='TODO'
+export=APP_KEY='TODO'
+```
+- Replace TODO with corresponding keys
+
+## Technical Difficulties
--- main.py
+++ main.py
@@ -5,15 +5,36 @@ import os
 
 
 def get_food_quote(twitter_api, food_query):
+    """
+    Retreives a random tweet from a list of 100 tweets based on the food_query
+    and gives the tweet text and info about it.
+    
+    
+    Parameters:
+    twitter_api (API): twitter api object
+    food_query (string): search terms for specific recipe or food
+    
+    Returns:
+    tuple: Tweet text, twitter handle, exact time posted
+    
+    """
     twitter_query = api.search(food_query, count=100)
 
     random_tweet = twitter_query[random.randint(0,100)]
 
     t = api.get_status(random_tweet.id, tweet_mode="extended")
     try:
-        return (t.retweeted_status.full_text, f"@{t.retweeted_status.author._json['screen_name']}", t.retweeted_status.created_at)
+        return (
+            t.retweeted_status.full_text, 
+            f"@{t.retweeted_status.author._json['screen_name']}", 
+            t.retweeted_status.created_at
+        )
     except AttributeError:
-        return (t.full_text, f"@{t.author._json['screen_name']}", t.created_at)
+        return (
+            t.full_text, 
+            f"@{t.author._json['screen_name']}", 
+            t.created_at
+        )
 
 
 api_key = os.environ['CONSUMER_API_KEY']
@@ -28,8 +49,8 @@ except tweepy.TweepError:
     print("Tweepy Error, check API keys!")
     exit(1)
 
-api = tweepy.API(auth)
 
+api = tweepy.API(auth)
 app = flask.Flask(__name__)
 
 @app.route("/")
